d66f1f7042936f88fb13c3c65d3bdddd4e34cbb8
tatynha-chacon <tatynha.chacon@gmail.com>
2021-01-14 14:50:01 -0300

    first commit

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-Treinamento - Deploy simples
+<p>Example of <b>Texto em negrito</b></p>
+<p>Example of <em>Texto enfatizado</em></p>
+<p>Example of <strong>Texto forte</strong></p>
+<p>Example of <i>Texto em italico</i></p>
+<p>Example of <sup>Texto superior</sup></p>
+<p>Example of <sub>Texto inferior</sub></p>
+<p>Example of <del>Texto taxado</del></p>
+<p>Example of <code>Texto de código de computador</code></p>
